2faeb3dc3527c6d911994f152bb6e38e44ce1764
Wisnia44 <tttttttttttttt7@gmail.com>
2020-05-21 10:10:44 +0200

    spamfilter endpoint added

--- src/antispam/views.py
+++ src/antispam/views.py
@@ -1,45 +1,59 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import JsonResponse
-from django.core import serializers
-from django.conf import settings
-import json
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import ValidationError
+from antispam.serializers import FilterSerializer
 
 
+def spamFilter(email_body, email_from, sensitivity, blacklist):
 
-def spamFilter(email_body, email_from, sensivity, blacklist):
-	
-		if email_from in blacklist:
-			return 1
-        df = pd.read_csv('spam.csv', encoding="latin-1")
-        df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-        df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
-        X = df['v2']
-        y = df['label']
-        cv = CountVectorizer()
-        X = cv.fit_transform(X) 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-        if sensivity == 'low':
-			clf = MultinomialNB(class_prior = [.1, .1])
-		elif: sensivity == 'medium':
-			clf = MultinomialNB(class_prior = [.1, .5])
-		else:
-			clf = MultinomialNB(class_prior = [.1, .8])
-		clf.fit(X_train,y_train)
-		clf.score(X_test,y_test)
-		
-        data = [email_body]
-        vect = cv.transform(data).toarray()
-        if clf.predict(vect) == 1:
-			return 1
-		else:
-			return 0
+    if email_from in blacklist:
+        return 1
+    df = pd.read_csv("src/spam.csv", encoding="latin-1")
+    df.drop(["Unnamed: 2", "Unnamed: 3", "Unnamed: 4"], axis=1, inplace=True)
+    df["label"] = df["v1"].map({"ham": 0, "spam": 1})
+    X = df["v2"]
+    y = df["label"]
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42
+    )
+    if sensitivity == "low":
+        clf = MultinomialNB(class_prior=[0.1, 0.1])
+    elif sensitivity == "medium":
+        clf = MultinomialNB(class_prior=[0.1, 0.5])
+    else:
+        clf = MultinomialNB(class_prior=[0.1, 0.8])
+    clf.fit(X_train, y_train)
+    clf.score(X_test, y_test)
+
+    data = [email_body]
+    vect = cv.transform(data).toarray()
+    if clf.predict(vect) == 1:
+        return 1
+    else:
+        return 0
+
+
+class SpamView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = FilterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.data
+
+        result = spamFilter(
+            data["email_body"],
+            data["email_from"],
+            data["sensitivity"],
+            data["blacklist"],
+        )
+        return Response(result)
--- src/tampon/urls.py
+++ src/tampon/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from antispam.views import SpamView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('filter/', SpamView.as_view()),
     path('', include('pages.urls')),
     path('api/pages/', include('pages.api.urls', namespace='api-pages'))
     ]
